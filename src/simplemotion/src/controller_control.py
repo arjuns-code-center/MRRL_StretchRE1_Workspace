5dd7707ffac0d15352973085cbd0cc0ce2d189bf
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-03-02 16:01:43 -0500

    update controller

--- src/simplemotion/src/controller_control.py
+++ src/simplemotion/src/controller_control.py
@@ -8,7 +8,6 @@ import stretch_body.robot as sb
 from stretch_body.hello_utils import *
 import stretch_body.xbox_controller as xc
 
-
 class Stretch_Move:
     def __init__(self):
         # Define robot object and start it up
@@ -123,144 +122,25 @@ class Stretch_Move:
         self.delay(1.0)
 
         servo.stop()
-class Keys:
-    def __init__(self):
-        self.quit = 0
-        self.sm = Stretch_Move()
 
-        self.key_state = {'w': False, 'a': False, 's': False, 'd': False,
-                        'q': False, 'e': False, 
-                        'z': False, 'c': False,
-                        'n': False, 'm': False, 
-                        'i': False, 'o': False, 
-                        'k': False, 'l': False, 
-                        'h': False, 'j': False,
-                        'f': False, 'g': False, 
-                        'v': False, 'b': False,
-                        't': False}
-        
-    def setKeyStates(self):
-        keys = pygame.key.get_pressed()
+def main():
+    xbox_controller = xc.XboxController()
+    xbox_controller.start()
+    sm = Stretch_Move()
 
-        self.key_state['w'] = keys[pygame.K_w]
-        self.key_state['a'] = keys[pygame.K_a]
-        self.key_state['s'] = keys[pygame.K_s]
-        self.key_state['d'] = keys[pygame.K_d]
+    while True:
+        controller_state = xbox_controller.get_state()
 
-        self.key_state['q'] = keys[pygame.K_q]
-        self.key_state['e'] = keys[pygame.K_e]
-        self.key_state['z'] = keys[pygame.K_z]
-        self.key_state['c'] = keys[pygame.K_c]
-
-        self.key_state['n'] = keys[pygame.K_n]
-        self.key_state['m'] = keys[pygame.K_m]
-        self.key_state['i'] = keys[pygame.K_i]
-        self.key_state['o'] = keys[pygame.K_o]
-
-        self.key_state['k'] = keys[pygame.K_k]
-        self.key_state['l'] = keys[pygame.K_l]
-        self.key_state['h'] = keys[pygame.K_h]
-        self.key_state['j'] = keys[pygame.K_j]
-
-        self.key_state['f'] = keys[pygame.K_f]
-        self.key_state['g'] = keys[pygame.K_g]
-        self.key_state['v'] = keys[pygame.K_v]
-        self.key_state['b'] = keys[pygame.K_b]
-
-        self.key_state['t'] = keys[pygame.K_t]
-    
-    def queueActions(self):
-        # Moving forward/backward
-        if self.key_state['w']:
-            self.sm.move_base(0.1)
-        
-        if self.key_state['s']:
-            self.sm.move_base(-0.1)
-
-        # Turning left/right
-        if self.key_state['a']:
-            self.sm.rotate_base(-0.2)
-        
-        if self.key_state['d']:
-            self.sm.rotate_base(0.2)
-
-        # Arm extend/retract
-        if self.key_state['q']:
-            self.sm.move_arm_incremental(0.02)
-        
-        if self.key_state['e']:
-            self.sm.move_arm_incremental(-0.02)
-
-        # Lift up/down
-        if self.key_state['z']:
-            self.sm.move_lift_incremental(0.02)
-        
-        if self.key_state['c']:
-            self.sm.move_lift_incremental(-0.02)
-
-        if self.key_state['k']:
-            self.sm.controlEOA('wrist_yaw', 5)
-        
-        if self.key_state['l']:
-            self.sm.controlEOA('wrist_yaw', -5)
-
-        if self.key_state['f']:
-            self.sm.controlEOA('wrist_pitch', 5)
-        
-        if self.key_state['g']:
-            self.sm.controlEOA('wrist_pitch', -5)
-
-        if self.key_state['v']:
-            self.sm.controlEOA('wrist_roll', 5)
-        
-        if self.key_state['b']:
-            self.sm.controlEOA('wrist_roll', -5)
-
-        if self.key_state['n']:
-            self.sm.controlHead('head_pan', 10)
-        
-        if self.key_state['m']:
-            self.sm.controlHead('head_pan', -10)
-
-        if self.key_state['i']:
-            self.sm.controlHead('head_tilt', 30)
-        
-        if self.key_state['o']:
-            self.sm.controlHead('head_tilt', -30)
-
-        if self.key_state['h']:
-            self.sm.move_gripper(90)
-        
-        if self.key_state['j']:
-            self.sm.move_gripper(-90)
-
-        if self.key_state['t']:
-            print("Stopping robot")
-            self.sm.robot_stop()
-            self.quit = 1
-
-    def execCommand(self):
-        self.sm.execCommand()
-
-    def isStopped(self):
-        return self.quit
-
-print("========STRETCH Keyboard Controls========")
-print("Use WASD to move the base")
-print("Use Q and E to extend or retract the arm")
-print("Use Z and C to move lift up and down")
-print("Use N and M to pan head")
-print("Use I and O to tilt head")
-print("Use K and L to turn the wrist")
-print("Use H and J to move gripper")
-print("Use F and G to control wrist pitch")
-print("Use V and B to control wrist roll")
-print("Use T to stop robot")
-
-kb = Keys()
-
-while not kb.isStopped():
-    kb.setKeyStates()
-    kb.queueActions()
-    kb.execCommand()
-    time.sleep(0.1)
+        # TODO: finish all action listners
+        if not robot.is_calibrated():
+            manage_calibration(robot, controller_state)
+        else:
+            manage_base(robot, controller_state)
+            manage_lift_arm(robot, controller_state)
+            manage_end_of_arm(robot, controller_state)
+            manage_head(robot, controller_state)
+            manage_stow(robot, controller_state)
+            manage_image_capture(robot, controller_state)
+        manage_shutdown(robot, controller_state)
+        robot.push_command()
+        time.sleep(0.05)
