f36aa889f2e5442acbb1d9ec0bd73fba8290a6bc
Arjun Viswanathan <arjun.viswanathan2612@gmail.com>
2023-08-15 15:00:51 -0400

    added object detection code. Needs testing

--- src/simplemotion/src/video_object_detection.py
+++ src/simplemotion/src/video_object_detection.py
@@ -1,4 +1,4 @@
-from imageai.Detection import VideoObjectDetection
+from imageai.Detection import ObjectDetection
 import os
 import cv2
 import requests
@@ -8,26 +8,38 @@ retinanet_url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.
 yolov3_url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/yolov3.pt'
 tinyyolo_url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/tiny-yolov3.pt'
 
-camera = cv2.VideoCapture(0)
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
 
-detector = VideoObjectDetection()
-detector.setModelTypeAsRetinaNet()
-
-r = requests.get(retinanet_url, allow_redirects=True)
-# r = requests.get(yolov3_url, allow_redirects=True)
+# r = requests.get(retinanet_url, allow_redirects=True)
+r = requests.get(yolov3_url, allow_redirects=True)
 # r = requests.get(tinyyolo_url, allow_redirects=True)
 
-open('models/retinanet_resnet50_fpn_coco-eeacb38b.pth', 'wb').write(r.content)
-# open('models/yolov3.pt', 'wb').write(r.content)
+# open('models/retinanet_resnet50_fpn_coco-eeacb38b.pth', 'wb').write(r.content)
+open('models/yolov3.pt', 'wb').write(r.content)
 # open('models/tiny-yolov3.pt', 'wb').write(r.content)
 
-detector.setModelPath(os.path.join(execution_path , "models/retinanet_resnet50_fpn_coco-eeacb38b.pth"))
-# detector.setModelPath(os.path.join(execution_path , "models/yolov3.pt"))
+# detector.setModelPath(os.path.join(execution_path , "models/retinanet_resnet50_fpn_coco-eeacb38b.pth"))
+detector.setModelPath(os.path.join(execution_path , "models/yolov3.pt"))
 # detector.setModelPath(os.path.join(execution_path , "models/tiny-yolov3.pt"))
 
 detector.loadModel()
+custom_objects = detector.CustomObjects(person=True)
 
-video_path = detector.detectObjectsFromVideo(
-                camera_input=camera,
-                output_file_path=os.path.join(execution_path, "camera_detected_video"),
-                frames_per_second=20, log_progress=True, minimum_percentage_probability=40)
+camera = cv2.VideoCapture(0)
+success = 1
+
+print("Reading from camera...\n")
+while success:
+    success, image = camera.read()
+
+    detections = detector.detectObjectsFromImage(
+                    custom_objects=custom_objects,
+                    input_image=image,
+                    output_image_path=os.path.join(execution_path, "detected.jpg"),
+                    minimum_percentage_probability=40)
+    
+    for eachObject in detections:
+        print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+        print("--------------------------------")
+print("Camera terminated. Finished reading!\n")
