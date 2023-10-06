84d9ba344eda3b9598229efd45794b342978f54f
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-10-05 23:28:26 -0400

    added cmd_vel publishers

--- src/simplemotion/src/commands/moveBack.py
+++ src/simplemotion/src/commands/moveBack.py
@@ -1,5 +1,6 @@
 import stretch_body.robot as sb
 import rospy
+from geometry_msgs.msg import Twist
 import sys, tty, termios
 
 def getkeystroke():
@@ -14,24 +15,35 @@ def getkeystroke():
 
     return ch
 
-def callback(r, v, a):
-    while True:
-        key = getkeystroke()
+# def callback(r, v, a):
+#     while True:
+#         key = getkeystroke()
 
-        r.base.translate_by(x_m=-0.15, v_m=v, a_m=a)
-        r.push_command()
+#         r.base.translate_by(x_m=-0.15, v_m=v, a_m=a)
+#         r.push_command()
 
-        if key == 't' or key == 'T':
-            r.stop()
-            rospy.signal_shutdown('end of execution')
+#         if key == 't' or key == 'T':
+#             r.stop()
+#             rospy.signal_shutdown('end of execution')
 
 if __name__ == '__main__':
     rospy.init_node('move_back')
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
-    r = sb.Robot()
-    r.startup()
+    cmd = Twist()
+    cmd.linear.x = -1.0
+
+    while True:
+        pub.publish(cmd)
+        key = getkeystroke()
+
+        if key == 't' or key == 'T':
+            rospy.signal_shutdown('end of execution')
+
+    # r = sb.Robot()
+    # r.startup()
     v = 10.0
     a = 5.0
 
-    callback(r, v, a)
-    rospy.spin()
+    # callback(r, v, a)
+    # rospy.spin()
