587325c7ca1e1d3df0dbe9d451df116a89def512
Arjun Viswanathan <41523488+arjuns-code-center@users.noreply.github.com>
2023-10-23 16:35:49 -0400

    got followMarker working. Needs cleanup

--- src/image_capture/src/commands/followMarker.py
+++ src/image_capture/src/commands/followMarker.py
@@ -1,6 +1,6 @@
 # Author: Arjun Viswanathan
 # Date created: 4/13/23
-# Last modified date: 10/2/23
+# Last modified date: 10/23/23
 # Summary: follows a single ArUco marker in front of it using computer vision
 
 # How to run from command line:
@@ -15,26 +15,26 @@
 import time
 import argparse
 import cv2
-import scipy.io as sio
 import numpy as np
+import pyrealsense2 as rs
 
 # Import ROS specific packages
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, LaserScan
 import stretch_body.robot as sb
 from cv_bridge import CvBridge, CvBridgeError
 
 class FollowMarker:
     def __init__(self, robot, timer=False):
         self.start_time = time.time()
-        print("Starting Follow Object Algorithm...")
+        print("Starting Follow Marker Algorithm...")
 
         self.robot = robot
         self.base = self.robot.base
         self.timer = timer
 
         # Load camera parameters from MATLAB
-        self.path = "~/motion_ws/src/image_capture/src/calibration/"
+        # self.path = "~/motion_ws/src/image_capture/src/calibration/"
 
         # camParams = sio.loadmat(self.path + "d435i_camParams.mat")
         self.cameraMatrix = np.float32([[295.6886, 0, 321.6002], [0, 297.0698, 229.0389], [0, 0, 1]])
@@ -51,17 +51,27 @@ class FollowMarker:
         self.a = 5.0
         self.timeout = 1
 
-        self.distance = 500 # mm
-        self.ignore = 300
+        self.distance = 300 # mm
+        self.trans_ignore = 200
+        self.rot_ignore = 80
 
         self.j = 0
 
-        self.rgb_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.takeAction)
+        self.pipe = rs.pipeline()
+        cfg = rs.config()
+        w = 640
+        h = 480
+        cfg.enable_stream(rs.stream.color, w, h, rs.format.bgr8, 30)
+        cfg.enable_stream(rs.stream.depth, w, h, rs.format.z16, 30)
+        self.pipe.start()
+
+        self.sub = rospy.Subscriber('/scan', LaserScan, self.takeAction)
         rospy.spin()
 
-    def takeAction(self, rgb_img):
+    def takeAction(self, msg):
         if self.timer:
             if (time.time() - self.start_time) > 30:
+                self.pipe.stop()
                 self.robot.stop()
                 rospy.signal_shutdown("Ending autonomous mode...")
                 
@@ -69,65 +79,73 @@ class FollowMarker:
         xr = 0
         mp = [0, 0]
         depth = 0
+        mid = 0
+
+        frame = self.pipe.wait_for_frames()
+        rgb_frame = frame.get_color_frame()
+
+        rgb_img = np.asanyarray(rgb_frame.get_data())
 
         try:
-            cv2_rgbimg = CvBridge().imgmsg_to_cv2(rgb_img, 'bgr8')
+            # cv2_rgbimg = CvBridge().imgmsg_to_cv2(rgb_img, 'bgr8')
 
-            if cv2_rgbimg is not None:
-                s = cv2_rgbimg.shape # (height, width, channels)
+            if rgb_img is not None:
+                s = rgb_img.shape # (height, width, channels)
 
-                (corners, ids, rejected) = self.detector.detectMarkers(cv2_rgbimg)
+                (corners, ids, rejected) = self.detector.detectMarkers(rgb_img)
 
                 if len(corners) > 0:
                     ids = ids.flatten()
 
                     # For every detected marker, we do pose estimation using its corners and find the rotational and translational vectors
                     for (markerCorner, markerID) in zip(corners, ids):
-                        reshapedCorners = markerCorner.reshape((4, 2))
-                        (tL, tR, bR, bL) = reshapedCorners
-                        tL = [int(tL[0]), int(tL[1])]
-                        tR = [int(tR[0]), int(tR[1])]
-                        bR = [int(bR[0]), int(bR[1])]
-                        bL = [int(bL[0]), int(bL[1])]
+                        if markerID == 16:
+                            reshapedCorners = markerCorner.reshape((4, 2))
+                            (tL, tR, bR, bL) = reshapedCorners
+                            tL = [int(tL[0]), int(tL[1])]
+                            tR = [int(tR[0]), int(tR[1])]
+                            bR = [int(bR[0]), int(bR[1])]
+                            bL = [int(bL[0]), int(bL[1])]
 
-                        # mp[0] = int((tL[0] + bR[0]) / 2)
-                        # mp[1] = int((tL[1] + bR[1]) / 2)
-                        
-                        rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorner, self.markerLength, self.cameraMatrix, self.distCoeffs)
-                        rvec = rvec[0][0]
-                        tvec = tvec[0][0]
+                            # mp[0] = int((tL[0] + bR[0]) / 2)
+                            # mp[1] = int((tL[1] + bR[1]) / 2)
+                            
+                            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorner, self.markerLength, self.cameraMatrix, self.distCoeffs)
+                            rvec = rvec[0][0]
+                            tvec = tvec[0][0]
 
-                        mp[0] = tvec[0]
-                        mp[1] = tvec[1]
+                            mp[0] = tvec[0]
+                            mp[1] = tvec[1]
 
-                        depth = round(tvec[2], 2) # mm
+                            depth = round(tvec[2], 2) # mm
 
-                        # Printing distance on the image
-                        cv2.putText(cv2_rgbimg, str(depth), (tL[0], tL[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                        print("Marker detected! ID: {}, Center: {}, Distance (mm): {}".format(str(markerID), mp, depth))
+                            # Printing distance on the image
+                            # cv2.putText(rgb_img, str(depth), (tL[0], tL[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                            print("Marker {} detected! X: {}, Y: {}, Distance (mm): {}".format(str(markerID), mp[0], mp[1], depth))
+                        else:
+                            print("Marker {} detected!")
 
                     # Press 's' key when detecting marker to save image. Only available when marker is detected
                     if cv2.waitKey(33) == ord('s'):
                         print("Taking ArUco pic {}...".format(j))
-                        cv2.imwrite(self.path + "Images/aruco_image_{}.png".format(self.j), cv2_rgbimg)
+                        cv2.imwrite(self.path + "Images/aruco_image_{}.png".format(self.j), rgb_img)
                         self.j += 1
             else:
                 print("No marker detected :(")
         except CvBridgeError as e:
             rospy.logwarn('CV Bridge Error: {0}'.format(e))
 
-        if depth > self.ignore:
+        if depth != 0:
             if depth > self.distance:
                 xm = self.moveBy
-            elif depth < self.distance:
+            elif depth < self.trans_ignore:
                 xm = -self.moveBy
 
         # If it detected a marker, mp != 0
-        # if mp[0] != 0 and mp[1] != 0:
-        #     if mp[0] < 0:
-        #         xr = self.rotBy
-        #     elif mp[0] > 0:
-        #         xr = -self.rotBy
+        if mp[1] < -self.rot_ignore:
+            xr = -self.rotBy
+        elif mp[1] > self.rot_ignore:
+            xr = self.rotBy
 
         if xm != 0:
             self.move_base(xm)
@@ -155,7 +173,7 @@ class FollowMarker:
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument('--timer', default=False, type=str, help='what avoidance algorithm to run')
+    args.add_argument('--timer', default=False, type=str, help='do you want a timer of 30 seconds?')
     args, unknown = args.parse_known_args()
     timer = int(args.timer)
 
